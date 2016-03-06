99c30ca1ca7687d91457e0a2707c136a72acd661
bliu72 <bliu72@gmail.com>
2016-03-06 16:40:33 -0500

    Adding DELETE /users/login

--- middleware.js
+++ middleware.js
@@ -1,16 +1,41 @@
+var cryptojs = require('crypto-js');
+
 module.exports = function(db) {
 
 	return {
 		requireAuthentication: function(req, res, next) {
-			var token = req.get('Auth');
-			console.log(token);
-			db.user.findByToken(token).then(function(user) {
+			var token = req.get('Auth') || '';
+			console.log('---> ' + token);
+
+			db.token.findOne({
+				where: {
+					tokenHash: cryptojs.MD5(token).toString()
+				}
+			}).then(function(tokenInstance) {
+				if (!tokenInstance) {
+					throw new Error();
+				}
+				console.log("found token tokenInstance");
+
+				req.token = tokenInstance;
+				return db.user.findByToken(token);
+			}).then(function(user) {
+				console.log("found token token");
 				req.user = user;
 				next();
-			}, function(e) {
+			}).catch(function(e) {
+				console.log("not found token: " + e);
 				res.status(401).send();
 			});
-		}
+
+			// db.user.findByToken(token).then(function(user) {
+			// 	req.user = user;
+			// 	next();
+			// }, function(e) {
+			// 	res.status(401).send();
+			// });
+		},
+
 	};
 
 }
--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	}).then(function(todos) {
 		res.json(todos);
 	}, function(e) {
+		console.log(e);
 		res.status(500).send();
 	});
 
@@ -221,16 +222,24 @@ app.post('/users', function(req, res) {
 
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authenticate');
+		userInstance = user;
+		return db.token.create({
+			token: token
+		});
 
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			res.status(401).send();
-		}
-	}, function(e){
+		// if (token) {
+		// 	res.header('Auth', token).json(user.toPublicJSON());
+		// } else {
+		// 	res.status(401).send();
+		// }
+	}).then(function(tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function(e){
+		console.log(e);
 		res.status(401).send();
 	});
 
@@ -251,6 +260,14 @@ app.post('/users/login', function(req, res) {
 	// }, function(e) {
 	// 	res.status(500).send();
 	// });
+});
+
+app.delete('/users/login', middleware.requireAuthentication, function(req, res) {
+	req.token.destroy().then(function() {
+		res.status(204).send();
+	}).catch(function() {
+		res.status(500).send();
+	});
 });
 
 db.sequelize.sync().then(function() {
